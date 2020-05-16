da928975491ce2abc958d07914031c42b41b8f63
Gergo <gergo@gmail.com>
2020-05-16 20:39:01 +0100

    fileMerger.js for increaseVideo.json files

---
+++ fileMerger.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+let nextFile = true
+let nextFileNumber = 1
+let videosData = []
+while (nextFile) {
+    const path = `framesProcessed/#${nextFileNumber}increaseVideo.json`
+
+    if (fs.existsSync(path)) {
+        let videosDataFromFile = JSON.parse(fs.readFileSync(path))
+        videosData = videosData.concat(videosDataFromFile)
+        nextFileNumber++
+    } else {
+        nextFile = false
+    }
+}
+
+fs.writeFile('framesProcessed/mergedIncreaseVideo.json', JSON.stringify(videosData), function (err, data) {
+    console.log("merge done, with " + videosData.length + " videos")
+})
